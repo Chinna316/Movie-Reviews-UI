38c3893c46ef60f553f2b6171cd0570f4249791a
chinna <chianna.annepu316@gmail.com>
2023-03-24 16:15:29 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './home/Home';
 
 function App() {
-  return (
+
+  const [Movies, setMovies] = useState()
+    const getMovies = async() => {
+      try {
+        const response = await api.get('/Movies');
+        setMovies(response.data);
+      }
+      catch(error) {
+        console.log("error");
+      }
+    }
+
+    useEffect(() => {
+      getMovies();
+    }, [])
+    
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Layout />}></Route>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
