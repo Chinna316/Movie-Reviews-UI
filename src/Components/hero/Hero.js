f4944ff7134a8cb1437e0e72b167b27698ff2510
chinna <chianna.annepu316@gmail.com>
2023-04-21 18:15:42 -0500

    commit

--- src/Components/hero/Hero.js
+++ src/Components/hero/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Hero.css'
 import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 
 const Hero = ({Movies}) => {
   return (
@@ -12,13 +14,20 @@ const Hero = ({Movies}) => {
                     return(
                         <Paper>
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{'--img': 'url(${Movie.backdrops[0]})'}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={Movies.poster} alt='' />
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{Movies.title}</h4>
+                                        </div>
+                                        <div className='movie-button-container'>
+                                            <div className='play-button-icon-container'>
+                                                <FontAwesomeIcon className='play-button-icon'
+                                                icon={faCirclePlay}
+                                                />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
